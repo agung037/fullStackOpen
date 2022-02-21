ca3acafef62846d8773d5e71efaf7a8f766bec15
agung037 <agungk878@gmail.com>
2022-02-21 19:51:07 +0700

    manual state (not recommended)

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,6 +1,18 @@
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 import App from './App'
 
+let counter = 1
 
-ReactDOM.render(<App />,document.getElementById('root'))
+const refresh = () => {
+    ReactDOM.render(<App counter={counter}/>, document.getElementById('root'))    
+}
 
+setInterval(() => {
+    refresh()
+    counter += 1
+}, 100)
+
+
+// ReactDOM.render(
+//     <App counter={counter} />, document.getElementById('root')
+// )
