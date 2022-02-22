857c1b8ff10f5893837ac8bf1914824d2f9aa6f6
agung037 <agungk878@gmail.com>
2022-02-22 11:24:52 +0700

    membuat counter

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,8 +1,46 @@
-const App = (props) => {
-  const {counter} = props
-  return (
-    <div>{counter}</div>
-  )
-}
+import {useState} from 'react'
 
+const App = () => {
+  let [counter, setCounter] = useState(0)
+
+  // setTimeout(
+  //   () => setCounter(counter + 1),
+  //   1000
+  // )
+
+  const handleClick = () => {
+    console.log("button clicked")
+    
+    if (counter >= 15)
+    {
+      setCounter(counter = 0)
+    }else{
+      setCounter(counter += 3)
+    }
+  }
+
+  const handleClickMinus = () => {
+    setCounter(counter - 1)
+  }
+
+  console.log("rendering ", counter)
+
+  return(
+    <div>
+
+      <p>
+        {counter}
+      </p>
+
+      <button onClick={handleClick}> 
+        benar +3
+      </button>
+      
+      <button onClick={handleClickMinus}>
+        salah -1
+      </button>
+    </div>
+  )
+
+}
 export default App
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,18 +1,6 @@
 import ReactDOM from 'react-dom';
 import App from './App'
 
-let counter = 1
-
-const refresh = () => {
-    ReactDOM.render(<App counter={counter}/>, document.getElementById('root'))    
-}
-
-setInterval(() => {
-    refresh()
-    counter += 1
-}, 100)
-
-
-// ReactDOM.render(
-//     <App counter={counter} />, document.getElementById('root')
-// )
+ReactDOM.render(
+    <App/>, document.getElementById('root')
+)
