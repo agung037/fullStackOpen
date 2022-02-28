56ee7b6567cd2497f2208ba34e48f05a9dbf90f8
agung037 <agungk878@gmail.com>
2022-02-28 15:26:15 +0700

    Finish Form Exercises 2.6.-2.10.

--- part2/b-forms/src/App.js
+++ part2/b-forms/src/App.js
@@ -1,54 +1,73 @@
-import { useState } from "react";
-import Note from "./components/Note";
+import { useState } from "react"
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNotes] = useState('')
-  const [showAll, setShowAll] = useState(false)
+const App = () => {
+  const [persons, setPersons] = useState([    
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    ])
+  const [newContact, setNewContact] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [find, setFind] = useState('')
+  const [findResult, setFindResult] = useState(persons)
+  
 
-  const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
 
-  const addNote = (event) => {
+  const submitHandler = (event) => {
     event.preventDefault();
 
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1
+    // check for duplicate if not duplicate proceed
+    if (!persons.map(x => x.name ).includes(newContact)){
+      const contactObject = {
+        id: persons.length + 1,
+        name: newContact,
+        number: newNumber
+      }
+      setPersons(persons.concat(contactObject))
+      setNewContact('')
+      setNewNumber('')
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNotes('')
+    // if duplicate send an alert
+    else{
+      alert(`${newContact} is already exsist in phonebook`)
+    }
+  } 
 
+  const handleWriteContact = (event) => {
+      setNewContact(event.target.value)
   }
 
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNotes(event.target.value)
+  const handleWriteNumber = (event) => {
+    setNewNumber(event.target.value)
   }
+
+  const handlerFind = (event) => {
+    setFind(event.target.value)
+    if (event.target.value){
+      let result = persons.filter(p => p.name.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1)
+      setFindResult(result)
+    }else{
+      setFindResult(persons)
+    }       
+  }
+
+  
 
   return (
     <div>
-      <h1>Note</h1>
-
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-            show {showAll ? 'important' : 'all'}
-        </button>
-      </div>
-
-      <ul>
-        {notesToShow.map(note => <Note key={note.id} note={note} />)}
-      </ul>
-     
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange}/>
-        <button type="submit">save</button>
-      </form>
+      <h2>Phonebook</h2>
+      <Filter handlerFind={handlerFind} find={find} />
+      <h2>Add New</h2>
+      <PersonForm submitHandler={submitHandler} newContact={newContact} handleWriteContact={handleWriteContact} handleWriteNumber={handleWriteNumber} newNumber={newNumber}/>
+      <h2> Numbers </h2>
+      <Persons find={find} findResult={findResult} persons={persons}/>
     </div>
   )
 }
-
 
 export default App
